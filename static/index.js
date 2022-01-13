873a6c124517b63c7fb64632566a84b3c30cee5d
devenmatthews <70613391+devenmatthews@users.noreply.github.com>
2022-01-13 01:17:08 -0800

    basic CRUD operations

---
+++ static/index.js
@@ -0,0 +1,54 @@
+var table_length = 0
+var selected_item = ""
+
+function submitForm(){
+    formData = getFormData();
+    if (formData ==  "") {
+        alert("Item name is a required field");
+    } else {
+        if (selected_item == "") {
+            addToTable(formData);
+        } else {
+            updateItem(formData);
+        }
+    }   
+    clearForm();
+}
+
+function getFormData() {
+    item_name = document.getElementById("new_item").value;
+    return item_name
+}
+
+function clearForm() {
+    document.getElementById("new_item").value = "";
+}
+
+function deleteItem(_this) {
+    if(confirm("Delete Record: Are you sure?")) {
+        _item = _this.parentElement.parentElement;
+        document.getElementById("inventory_list").deleteRow(_item.rowIndex);
+        table_length -= 1;
+    }
+}
+
+function editItem(_edit_button) {
+    selected_item = _edit_button.parentElement.parentElement;
+    document.getElementById("new_item").value = selected_item.cells[0].innerHTML;
+}
+
+function updateItem(form_data) {
+    selected_item.cells[0].innerHTML = form_data;
+    selected_item = "";
+}
+
+function addToTable(_item) {
+    var _table = document.getElementById("inventory_list").getElementsByTagName('tbody')[0];
+    var _row = _table.insertRow(table_length);
+    _cell = _row.insertCell(0);
+    _cell.innerHTML = _item;
+    _cell_buttons = _row.insertCell(1);
+    _cell_buttons.innerHTML = ` <input type="button" value="Edit" onclick="editItem(this)"></input>
+                                <input type="button" value="Delete" onclick="deleteItem(this)"></input>`;
+    table_length += 1;
+}
